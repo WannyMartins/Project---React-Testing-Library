fd575873cbf3cee53a7e0ecff362e5464dbbaa66
Wanny Martins <wcatery@gmail.com>
2022-03-22 17:20:39 -0300

    commit teste do reauisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import React from 'react';
+import { Pokedex } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testando About', () => {
+  test(`Teste se a página contém um heading "h2" com o texto 
+    "Encountered pokémons"`, () => {
+    render(<Pokedex />);
+    const textPokemon = screen.getByText(/Encountered pokémons/i);
+    expect(textPokemon).toBeInTheDocument();
+  });
+
+  // test('Teste se a página contém a imagem NotFound.', () => {
+  //   renderWithRouter(<NotFound />);
+  //   const imagemNotFound = screen
+  //     .getByAltText('Pikachu crying because the page requested was not found');
+  //   expect(imagemNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  // });
+});

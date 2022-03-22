643044b5a12bd6bcb52d3a5ea20ce5ad21dcf0e8
Wanny Martins <wcatery@gmail.com>
2022-03-22 16:36:50 -0300

    testando se nao tiver pokemons favoritos aparece a mensagem "No favorites pokemon found"

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test(`É exibido na tela a mensagem "No favorite pokemon found",
+se a pessoa não tiver pokémons favoritos.`, () => {
+  render(<FavoritePokemons />);
+  const mensagem = screen.getByText('No favorite pokemon found');
+  expect(mensagem).toBeDefined();
+});
